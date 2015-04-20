d0ff3d506c6666806cc9050cc1993cd570cbd0c8
VeritestAF15 <lionbridge1>
2015-04-20 13:16:05 +0530

    Sample Program
    To Test GIT Connection

---
+++ Sample_ProgforGIT/src/HeadStart_TC.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class HeadStart_TC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://google.co.in");
+	}
+
+}

2d13ec628ee29b98ac0fa24f89fe36a3643f04af
VeritestAF15 <lionbridge1>
2015-04-21 15:30:01 +0530

    New project
    Test

--- Sample_ProgforGIT/src/HeadStart_TC.java
+++ Sample_ProgforGIT/src/HeadStart_TC.java
@@ -6,8 +6,9 @@ public class HeadStart_TC {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://google.co.in");
+		//WebDriver driver = new FirefoxDriver();
+		//driver.get("http://google.co.in");
+		System.out.println("welcome");
 	}
 
 }
